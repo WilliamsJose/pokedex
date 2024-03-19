324d17adc9eab46f384c835be8326edadc992818
will <contatowilliamsj@gmail.com>
2024-03-19 13:48:47 -0300

    chore: testing husky

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,8 +10,8 @@ const Button = styled.button<ButtonProps>`
   border: 1px solid transparent;
   padding: 0.6em 1.2em;
   font-size: 1em;
-  font-weight: 500;
   font-family: inherit;
+  font-weight: 500;
   cursor: pointer;
   transition: border-color 0.25s;
   &:hover {
